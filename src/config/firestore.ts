32972bbe90de85665b1672d1ad0a18ec5ef4637d
masb0ymas <fajriajjha@gmail.com>
2021-03-28 17:42:07 +0700

    feat: add config firestore

--- src/config/firestore.ts
+++ src/config/firestore.ts
@@ -1,6 +1,22 @@
 import * as admin from 'firebase-admin'
+import path from 'path'
+import fs from 'fs'
 
-admin.initializeApp()
+const { FIREBASE_DATABASE_URL } = process.env
+
+const serviceAccountKey = path.resolve('./serviceAccountKey.json')
+
+if (!fs.existsSync(serviceAccountKey)) {
+  throw new Error(
+    'Missing serviceAccountKey!!!\nCopy serviceAccountKey from your console firebase to root directory "serviceAccountKey.json"'
+  )
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+  databaseURL: FIREBASE_DATABASE_URL,
+})
+
 const db = admin.firestore()
 
 export { db }
